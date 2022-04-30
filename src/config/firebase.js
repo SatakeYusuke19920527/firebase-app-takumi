d3b60f1ef98a6ffda0366d518486537c95107673
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-04-30 21:00:30 +0900

    new app

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { getApps,initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSEAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+}
+
+const apps =  getApps
+if (!apps.length){
+    initializeApp(firebaseConfig);
+}
+ 
---
+++ src/pages/CreateUser.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page1 = () => {
+    console.log('aaa')
+    return(
+        <h1>a</h1>
+    )
+
+}
+
+export default Page1
